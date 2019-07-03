c01cf085437bb0b482aabffb482dbc4e36dacf1c
I330563 <tamas.fenyvesi@sap.com>
2019-07-03 12:36:51 +0200

    New feature; printing environment variables to ui

--- service/app/hello.py
+++ service/app/hello.py
@@ -1,9 +1,21 @@
 from flask import Flask
 
+import os
+import socket
+
 app = Flask(__name__)
+
+msg = """<html><body style='{}'>
+Hello World from Python Flask!
+Currently we are in '{}'
+hostname is '{}'
+</body></html>"""
+
 
 @app.route('/hello')
 def helloIndex():
-    return '<html><body style="background: yellow;" > Hello World from Python Flask! </body></html>'
+    style = "background: " + str(os.environ.get('bgcolor'))
+    namespace = str(os.environ.get('NAMESPACE'))
+    return msg.format(style, namespace, socket.gethostname())
 
-app.run(host='0.0.0.0', port= 80)
+app.run(host='0.0.0.0', port=80)
